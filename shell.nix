e02cd69096a02164af1b14050ffdf9cb4ecc7cdf
Rune K. Svendsen <runesvend@gmail.com>
2023-09-29 17:24:23 +0200

    Pin release-23.05 to commit hash in shell.nix
    To fix CI error:
    ```
    error: hash mismatch in file downloaded from 'https://github.com/NixOS/nixpkgs/archive/release-23.05.tar.gz':
             specified: sha256:0n00rsh3lagsbqypsnjram29242kvlv9j0zx305zlm3nc7n1jxbx
             got:       sha256:0l1r8x3qd3z778nii2lw2c6dyb15xf1l06sck0jjj247dv951hiw
    ```

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
   pkgs =
     import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/release-23.05.tar.gz";
-      sha256 = "0n00rsh3lagsbqypsnjram29242kvlv9j0zx305zlm3nc7n1jxbx";
+      url = "https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+      sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
     }) {};
   release-21-05 =
     import (builtins.fetchTarball {
