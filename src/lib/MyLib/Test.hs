4956515e1da6fb810c3a15fa41b493ef97ce51e1
Rune K. Svendsen <runesvend@gmail.com>
2023-12-20 16:12:11 +0100

    Move test query logic into MyLib.Test
    TODO: Make Benchmark use this module (to avoid code duplication between test suite and benchmark).

---
+++ src/lib/MyLib/Test.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE OverloadedStrings #-}
+module MyLib.Test
+( allTestCases
+, case1
+, case2
+, case3
+, case4
+, QueryTest(..)
+, PPFunctions(..)
+)
+where
+
+import qualified MyLib
+import MyLib.Examples
+
+import Data.Functor (void)
+import Data.Maybe (fromMaybe)
+import qualified Data.ByteString.Char8 as BSC8
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Set as Set
+
+data QueryTest = QueryTest
+    { queryTest_name :: String
+    , queryTest_runQuery
+        :: MyLib.IDigraph MyLib.FullyQualifiedType (NE.NonEmpty MyLib.TypedFunction)
+        -> [PPFunctions]
+    , queryTest_expectedResult :: Set.Set PPFunctions
+    }
+
+mkTestCase
+  :: Int
+  -> Int
+  -> ((MyLib.FullyQualifiedType, String), (MyLib.FullyQualifiedType, String))
+  -> [BSC8.ByteString]
+  -> QueryTest
+mkTestCase k maxCount (from, to) expectedList =
+    QueryTest
+        { queryTest_name = unwords [snd from, "to", snd to]
+        , queryTest_runQuery = map (PPFunctions . map void) . getResults
+        , queryTest_expectedResult = Set.fromList $ fns expectedList
+        }
+  where
+    getResults graph =
+      take maxCount $ MyLib.runQueryAll k (fst from, fst to) graph
+
+    fns :: [BSC8.ByteString] -> [PPFunctions]
+    fns = map (PPFunctions . NE.toList . fn)
+
+    fn :: BSC8.ByteString -> NE.NonEmpty MyLib.UntypedFunction
+    fn bs = fromMaybe
+      (error $ "parseComposedFunctions: bad input: " <> BSC8.unpack bs)
+      (MyLib.parseComposedFunctions bs)
+
+allTestCases :: [QueryTest]
+allTestCases =
+  [ case1
+  , case2
+  , case3
+  , case4
+  ]
+
+case1 :: QueryTest
+case1 =
+  mkTestCase 1 1
+    (strictByteString, string)
+    ["bytestring-0.11.4.0:Data.ByteString.Char8.unpack"]
+
+case2 :: QueryTest
+case2 =
+    mkTestCase 1 1
+        (string, strictByteString)
+        ["bytestring-0.11.4.0:Data.ByteString.Char8.pack"]
+
+case3 :: QueryTest
+case3 =
+  mkTestCase 4 26
+    (lazyText, strictByteString)
+    [ "bytestring-0.11.4.0:Data.ByteString.Char8.pack . text-2.0.2:Data.Text.Lazy.unpack"
+    , "text-2.0.2:Data.Text.Encoding.encodeUtf16BE . text-2.0.2:Data.Text.Lazy.toStrict"
+    , "text-2.0.2:Data.Text.Encoding.encodeUtf32LE . text-2.0.2:Data.Text.Lazy.toStrict"
+    , "text-2.0.2:Data.Text.Encoding.encodeUtf8 . text-2.0.2:Data.Text.Lazy.toStrict"
+    , "bytestring-0.11.4.0:Data.ByteString.toStrict . text-2.0.2:Data.Text.Lazy.Encoding.encodeUtf16LE"
+    , "bytestring-0.11.4.0:Data.ByteString.toStrict . text-2.0.2:Data.Text.Lazy.Encoding.encodeUtf32BE"
+    , "bytestring-0.11.4.0:Data.ByteString.toStrict . text-2.0.2:Data.Text.Lazy.Encoding.encodeUtf8"
+    ]
+
+case4 :: QueryTest
+case4 =
+  mkTestCase 3 37
+    (strictByteString, lazyText)
+    [ "text-2.0.2:Data.Text.Lazy.pack . bytestring-0.11.4.0:Data.ByteString.Char8.unpack"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeASCII"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeLatin1"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf16BE"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf16LE"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf32BE"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf32LE"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf8"
+    , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf8Lenient"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.Char8.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.Lazy.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeASCII . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeLatin1 . bytestring-0.11.4.0:Data.ByteString.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeLatin1 . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf16BE . bytestring-0.11.4.0:Data.ByteString.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf16LE . bytestring-0.11.4.0:Data.ByteString.Char8.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf32BE . bytestring-0.11.4.0:Data.ByteString.Lazy.fromStrict"
+    , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf32LE . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
+    ]
+
+newtype PPFunctions = PPFunctions { unPPFunctions :: [MyLib.Function ()] }
+  deriving (Eq, Ord)
+
+instance Show PPFunctions where
+  show = MyLib.renderComposedFunctionsStr . unPPFunctions
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,98 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import qualified MyLib
-import MyLib.Examples
+import qualified MyLib.Test
 import Test.Hspec.Expectations.Pretty (shouldBe)
-import Data.Functor (void)
 import qualified Test.Hspec as HSpec
-import Data.Maybe (fromMaybe)
-import qualified Data.ByteString.Char8 as BSC8
-import qualified Data.List.NonEmpty as NE
 import qualified Data.Set as Set
 import Debug.Trace (trace)
+import Control.Monad (forM_)
 
 testDataFileName :: FilePath
 testDataFileName = "data/all3.json"
 
 main :: IO ()
 main = MyLib.withGraphFromFile testDataFileName $ \graph -> do
-  let getResults' k maxCount = traceFunction . map (PPFunctions . map void) . getResults k maxCount graph
-      testCase k maxCount (from, to) expectedList =
-        HSpec.it (unwords [snd from, "to", snd to]) $ do
-          getResults' k maxCount (fst from, fst to)
+  let testCase test =
+        HSpec.it (MyLib.Test.queryTest_name test) $ do
+          Set.fromList (traceFunction $ MyLib.Test.queryTest_runQuery test graph)
             `isSupersetOf`
-              fns expectedList
+              MyLib.Test.queryTest_expectedResult test
   HSpec.hspec $
     HSpec.describe "Unit tests" $ do
-      HSpec.describe ("Expected result contained in top query results") $ do
-        testCase 1 1
-          (strictByteString, string)
-            ["bytestring-0.11.4.0:Data.ByteString.Char8.unpack"]
-        testCase 1 1
-          (string, strictByteString)
-          ["bytestring-0.11.4.0:Data.ByteString.Char8.pack"]
-        testCase 4 26
-          (lazyText, strictByteString)
-          [ "bytestring-0.11.4.0:Data.ByteString.Char8.pack . text-2.0.2:Data.Text.Lazy.unpack"
-          , "text-2.0.2:Data.Text.Encoding.encodeUtf16BE . text-2.0.2:Data.Text.Lazy.toStrict"
-          , "text-2.0.2:Data.Text.Encoding.encodeUtf32LE . text-2.0.2:Data.Text.Lazy.toStrict"
-          , "text-2.0.2:Data.Text.Encoding.encodeUtf8 . text-2.0.2:Data.Text.Lazy.toStrict"
-          , "bytestring-0.11.4.0:Data.ByteString.toStrict . text-2.0.2:Data.Text.Lazy.Encoding.encodeUtf16LE"
-          , "bytestring-0.11.4.0:Data.ByteString.toStrict . text-2.0.2:Data.Text.Lazy.Encoding.encodeUtf32BE"
-          , "bytestring-0.11.4.0:Data.ByteString.toStrict . text-2.0.2:Data.Text.Lazy.Encoding.encodeUtf8"
-          ]
-        testCase 3 37
-          (strictByteString, lazyText)
-          [ "text-2.0.2:Data.Text.Lazy.pack . bytestring-0.11.4.0:Data.ByteString.Char8.unpack"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeASCII"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeLatin1"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf16BE"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf16LE"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf32BE"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf32LE"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf8"
-          , "text-2.0.2:Data.Text.Lazy.fromStrict . text-2.0.2:Data.Text.Encoding.decodeUtf8Lenient"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.Char8.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.Lazy.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf8 . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeASCII . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeLatin1 . bytestring-0.11.4.0:Data.ByteString.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeLatin1 . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf16BE . bytestring-0.11.4.0:Data.ByteString.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf16LE . bytestring-0.11.4.0:Data.ByteString.Char8.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf32BE . bytestring-0.11.4.0:Data.ByteString.Lazy.fromStrict"
-          , "text-2.0.2:Data.Text.Lazy.Encoding.decodeUtf32LE . bytestring-0.11.4.0:Data.ByteString.Lazy.Char8.fromStrict"
-          ]
+      HSpec.describe "Expected result contained in top query results" $
+        forM_ MyLib.Test.allTestCases testCase
   where
-    fns lst = map (PPFunctions . NE.toList . fn) lst
-
-    fn bs = fromMaybe
-      (error $ "parseComposedFunctions: bad input: " <> BSC8.unpack bs)
-      (MyLib.parseComposedFunctions bs)
-
-    getResults k maxResultCount graph (src, dst) =
-      take maxResultCount $ MyLib.runQueryAll k (src, dst) graph
-
     traceFunction = if shouldTrace then traceResults else id
     shouldTrace = False
 
-    traceResults :: [PPFunctions] -> [PPFunctions]
+    traceResults :: [MyLib.Test.PPFunctions] -> [MyLib.Test.PPFunctions]
     traceResults results =
-      trace (unlines $ "" : map show results) results
+        trace (unlines $ "" : map show results) results
 
-newtype PPFunctions = PPFunctions { unPPFunctions :: [MyLib.Function ()] }
-  deriving (Eq, Ord)
-
-instance Show PPFunctions where
-  show = MyLib.renderComposedFunctionsStr . unPPFunctions
-
-isSupersetOf :: (Show a, Ord a) => [a] -> [a] -> IO ()
-isSupersetOf superSetLst subSetLst =
-  let superSet = Set.fromList superSetLst
-      subSet = Set.fromList subSetLst
-  in Set.intersection superSet subSet `shouldBe` subSet
+isSupersetOf :: (Show a, Ord a) => Set.Set a -> Set.Set a -> IO ()
+isSupersetOf superSet subSet =
+  Set.intersection superSet subSet `shouldBe` subSet
