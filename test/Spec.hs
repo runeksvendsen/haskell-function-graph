3a09627160f1adc68eb7321efc0f80cbcd5897f8
Rune K. Svendsen <runesvend@gmail.com>
2024-04-19 08:12:21 +0200

    test: add `--trace` CLI argument

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use infix" #-}
 
 module Main where
 
@@ -8,17 +11,24 @@ import Test.Hspec.Expectations.Pretty (shouldBe)
 import qualified Test.Hspec as HSpec
 import qualified Data.Set as Set
 import Debug.Trace (trace)
-import Control.Monad (forM_)
+import Control.Monad (forM_, when)
 import qualified Control.Monad.ST as ST
 import qualified Data.Graph.Digraph as DG
 import qualified Data.List.NonEmpty as NE
 import Data.Functor (void)
+-- BEGIN: extra args
+import Data.List
+import qualified System.Environment as Arg
+import qualified Control.Exception as Ex
+import qualified System.Exit as Exit
+import qualified Test.Hspec.Runner as Hspec
+-- END: extra args
 
 testDataFileName :: FilePath
 testDataFileName = "data/all3.json"
 
 main :: IO ()
-main = FunGraph.withGraphFromFile FunGraph.defaultBuildConfig testDataFileName $ \graph -> do
+main = withTraceArg $ \shouldTrace -> FunGraph.withGraphFromFile FunGraph.defaultBuildConfig testDataFileName $ \graph -> do
   graphEdgeSet <- ST.stToIO (DG.toEdges graph)
   let graphEdges = Set.map void $ Set.fromList $ concat $ Set.map (NE.toList . DG.eMeta) graphEdgeSet
   let testCase test =
@@ -30,17 +40,31 @@ main = FunGraph.withGraphFromFile FunGraph.defaultBuildConfig testDataFileName $
                   FunGraph.Test.queryTest_expectedResult test
             graphEdges `isSupersetOf` ppFunctions
           HSpec.it "contained in top query results" $ do
-            result <- ST.stToIO $ FunGraph.Test.queryTest_runQuery test (FunGraph.runQueryTrace graph)
-            Set.fromList (map fst $ traceFunction result)
+            result <- ST.stToIO $ FunGraph.Test.queryTest_runQuery test (mkRunQueryFunction shouldTrace graph)
+            Set.fromList (map fst $ mkTraceFunction shouldTrace result)
               `isSupersetOf`
                 FunGraph.Test.queryTest_expectedResult test
-  HSpec.hspec $
+  runHspecWithTraceTip shouldTrace $
     HSpec.describe "Unit tests" $ do
       HSpec.describe "Expected result" $
         forM_ FunGraph.Test.allTestCases testCase
   where
-    traceFunction = if shouldTrace then traceResults else id
-    shouldTrace = True
+    traceCLIArg :: String
+    traceCLIArg = "--trace"
+
+    withTraceArg ioa =
+      withExtraArgs [(traceCLIArg, "print tracing information")] $ \args ->
+        ioa (args == [traceCLIArg])
+
+    mkTraceFunction shouldTrace = if shouldTrace then traceResults else id
+
+    mkRunQueryFunction shouldTrace = if shouldTrace then FunGraph.runQueryTrace else FunGraph.runQuery
+
+    runHspecWithTraceTip shouldTrace spec = do
+      summary <- Hspec.hspecResult spec
+      when (not (Hspec.isSuccess summary) && not shouldTrace) $
+        putStrLn $ "\nTest suite had failures. Run again with the " <> traceCLIArg <> " argument to print tracing information."
+      Hspec.evaluateSummary summary
 
     traceResults :: [(FunGraph.Test.PPFunctions, Double)] -> [(FunGraph.Test.PPFunctions, Double)]
     traceResults results =
@@ -49,3 +73,42 @@ main = FunGraph.withGraphFromFile FunGraph.defaultBuildConfig testDataFileName $
 isSupersetOf :: (Show a, Ord a) => Set.Set a -> Set.Set a -> IO ()
 isSupersetOf superSet subSet =
   Set.intersection superSet subSet `shouldBe` subSet
+
+-- | Pass extra arguments to an IO action that parses CLI arguments.
+--
+--   The use case is an IO action that does CLI argument parsing by itself,
+--   and would thus fail with "unknown argument" if you tried to run it with
+--   your custom argument.
+--
+--   Example:
+--
+-- >>> :{
+--  import qualified System.Environment as Arg
+--  import System.IO.Unsafe (unsafePerformIO)
+--  let myProgram = pure ()
+--  unsafePerformIO $
+--    Arg.withArgs ["--extra-test-arg", "--help"] $
+--      withExtraArgs [("--extra-test-arg", "extra test arg")] $ \extraArgs -> do
+--        getArgs <- Arg.getArgs
+--        pure (getArgs, extraArgs)
+-- :}
+withExtraArgs
+  :: [(String, String)]
+     -- ^ @extraArgs@: extra arguments: (option, help text)
+  -> ([String] -> IO a)
+     -- ^ argument: the list of options from @extraArgs@ that was passed as arguments.
+     --   the IO action is run without these arguments.
+  -> IO a
+     -- ^ IO action that is not passed the extra CLI arguments
+withExtraArgs extraArgs ioAction = do
+  args <- Arg.getArgs
+  let passedArgs = intersect args (map fst extraArgs)
+      remainingArgs = args \\ passedArgs
+  Arg.withArgs remainingArgs $ do
+    eitherA <- Ex.try $ ioAction passedArgs
+    when (args == ["--help"]) $
+      putStr $ unlines $
+          ""
+        : "EXTRA OPTIONS"
+        : map (\(option, helpText) -> "    " <> option <> "  " <> helpText) extraArgs
+    either (Ex.throwIO @Exit.ExitCode) pure eitherA
