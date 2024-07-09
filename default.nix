082e10370407b30cd5c0c9721ac0a2f3dcc3133c
Rune K. Svendsen <runesvend@gmail.com>
2024-07-09 19:11:30 +0200

    WIP: add default.nix
    TODOs:
    
    1. Make "dump-decls" build by not running the "test" test suite
    2. Runtime dep "graphviz"

---
+++ default.nix
@@ -0,0 +1,43 @@
+# TODO: runtime dep "graphviz"
+# TODO: make "dump-decls" build by not running the "test" test suite
+{ nixpkgs ? (import ./nix/pkgs.nix).pkgs
+, compiler ? "ghc92"
+}:
+let
+  bellman-ford = import (bellman-fordSrc + "/default.nix") { inherit nixpkgs compiler; };
+  bellman-fordSrc = builtins.fetchGit {
+    url = "https://github.com/runeksvendsen/bellman-ford.git";
+    rev = "8896ea261c29af4b69b6b8b007873c59c886618c";
+  };
+
+  dump-decls = nixpkgs.haskell.lib.setBuildTargets
+    (import (dump-declsSrc + "/default.nix") { inherit nixpkgs compiler; })
+    ["lib:lib" "test:doctest" "test:unit"];
+
+  dump-declsSrc = builtins.fetchGit {
+    url = "https://github.com/runeksvendsen/dump-decls.git";
+    rev = "8a2cdb42bf6fd8fc2508096a41a980dff0595a87";
+  };
+
+  servant-errors =
+    let servant-errorsSrc = builtins.fetchGit {
+          url = "https://github.com/epicallan/servant-errors.git";
+          rev = "7c564dff3574c35cae721b711bd90503b851438e";
+        };
+    in nixpkgs.pkgs.haskell.packages.${compiler}.callCabal2nix "servant-errors" servant-errorsSrc {};
+
+  args =
+    { bellman-ford = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage
+        (nixpkgs.pkgs.haskell.lib.overrideCabal bellman-ford)
+        { };
+
+      dump-decls = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage
+        (nixpkgs.pkgs.haskell.lib.overrideCabal dump-decls)
+        { };
+
+      servant-errors = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage
+        (nixpkgs.pkgs.haskell.lib.overrideCabal servant-errors)
+        { };
+    };
+in
+  nixpkgs.pkgs.haskell.packages.${compiler}.callCabal2nix "function-graph" ./. args
