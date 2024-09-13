c54a09b5972212f58950de0dcdcddfe38c2653bd
Rune K. Svendsen <runesvend@gmail.com>
2024-09-13 12:48:30 +0200

    benchmark-web: fail for "first result" unless result was found

--- benchmark/BenchmarkWeb.hs
+++ benchmark/BenchmarkWeb.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NumDecimals #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE LambdaCase #-}
 -- | Benchmark the web server
 --
 -- TODO: Make exe exit properly for CLI arguments that don't need server (e.g. --help, --list)
@@ -39,6 +40,9 @@ import Control.Monad.IO.Class (liftIO)
 import Lucid.Base (Html)
 import qualified Lucid.Base
 import qualified Data.Text.Lazy as LT
+import Streaming (lift)
+import Control.Monad (when)
+import Data.Maybe (isJust)
 
 testDataFileName :: FilePath
 testDataFileName = "data/all3.json"
@@ -116,11 +120,27 @@ runTests port = do
     untilFirstResult
       :: Stream (Of (Html ())) IO ()
       -> Stream (Of (Html ())) IO ()
-    untilFirstResult = do
+    untilFirstResult s = do
       let Lucid.Base.Attribute resultHtmlAttributeText _ = Server.Pages.Search.mkResultAttribute ""
           containsSearchResult :: Lucid.Html () -> Bool
           containsSearchResult = LT.isInfixOf (LT.fromStrict resultHtmlAttributeText) . Lucid.renderText
-      Streaming.Prelude.takeWhile (not . containsSearchResult)
+      maybeNotContainsSearchResult <- takeWhileMaybe (not . containsSearchResult) s
+      when (isJust maybeNotContainsSearchResult) $
+        fail "Search result not found"
+
+    -- Same as 'Streaming.Prelude.takeWhile' except returns 'Nothing' if the predicate returned False at some point
+    --  (so that the number of stream elements was reduced).
+    -- In other words: returns a 'Just' if the output stream is the same as the input stream.
+    takeWhileMaybe :: Monad m => (a -> Bool) -> Stream (Of a) m r -> Stream (Of a) m (Maybe r)
+    takeWhileMaybe thePred =
+      let loop str = lift (Streaming.Prelude.next str) >>= \case
+            Right (a, as) ->
+              if thePred a
+                then Streaming.Prelude.yield a >> loop as
+                else pure Nothing
+            Left r ->
+              pure $ Just r
+      in loop
 
 searchClientM
   :: (HtmlStream IO () -> Servant.Client.Streaming.ClientM (Lucid.Html ()))
