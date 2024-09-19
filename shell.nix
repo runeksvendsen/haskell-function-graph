43c565296c5880e8fd610aee49162a595c786859
Rune K. Svendsen <runesvend@gmail.com>
2024-09-19 12:36:49 +0200

    shell.nix: upgrade cabal-install

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
+let
+  pkgsCabal =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/0c924ab22aa8818c701ad9d94be34dc7934d692c.tar.gz";
+      sha256 = "1llhad9y24bqp7ib8ls2fwk4jpqakyrzhy7p2djkqlbsnhzcvd1q";
+    }) {};
+in
 with (import ./nix/pkgs.nix);
 pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.haskell.compiler.ghc90
-    release-21-05.cabal-install
+    pkgsCabal.cabal-install # use version of cabal-install with "multiple components"-support (https://github.com/haskell/cabal/pull/8726)
     pkgs.zlib.dev
     pkgs.pkg-config
     pkgs.git
