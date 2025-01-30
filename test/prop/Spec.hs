300573d8ee76ef7a029e152c5c4c4cbec5372dbf
Rune K. Svendsen <runesvend@gmail.com>
2025-01-30 15:12:51 +0100

    Use `Tasty.withResource` to read PrioTrie
    To not mess with `main` handling CLI options

--- test/prop/Spec.hs
+++ test/prop/Spec.hs
@@ -2,11 +2,16 @@ module Main (main) where
 
 import qualified Spec.PrioTrie
 import qualified Test.Tasty as Tasty
+import qualified System.IO.Unsafe as Unsafe
 
 main :: IO ()
 main = do
-  prioTrieSpecSetup <- Spec.PrioTrie.setup
   Tasty.defaultMain $
     Tasty.testGroup "Properties"
-      [ Spec.PrioTrie.spec prioTrieSpecSetup
+      [ Tasty.withResource Spec.PrioTrie.setup mempty $
+          -- NOTE: For some reason, 'Tasty.withResource' provides the resource to the 'TestTree' as an IO action.
+          --       We get around this by using 'unsafePerformIO'. Would be nice if tasty didn't require this.
+          --       Note the docs for 'Tasty.withResource' say that the "setup" IO action will be evaluated
+          --       only once regardless of how many times the IO action below is evaluted.
+          Spec.PrioTrie.spec . Unsafe.unsafePerformIO
       ]
