3ee3922e970b71eb9ca64587babe6d04b4472bdc
Rune K. Svendsen <runesvend@gmail.com>
2025-04-05 17:41:52 +0200

    Move tracing to `function-graph` exe
    Run `cabal run function-graph -- data/new-forall.json` to test

--- app/Main.hs
+++ app/Main.hs
@@ -1,98 +1,96 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
 module Main (main) where
 
-import qualified FunGraph
-import qualified FunGraph.Util
-import qualified System.Environment as Env
-import qualified Control.Monad.ST as ST
-import qualified Data.Graph.Digraph as DG
-import qualified System.Timeout
-import Control.Monad (when, forM_)
-import Data.Maybe (fromMaybe)
-import Text.Read (readMaybe)
-import qualified Data.Set as Set
-import qualified System.Console.ANSI as ANSI
-import qualified Data.List.NonEmpty as NE
+import FunGraph.Types
+import qualified Json
+import qualified Types
 import qualified Data.Text as T
-import qualified FunGraph.Types as Types
-
-buildConfig :: FunGraph.BuildConfig
-buildConfig = FunGraph.emptyBuildConfig
-  { FunGraph.buildConfig_excludeTypes = Set.fromList $ map Types.parsePprTyConSingleton
-      [ "[ghc-prim-0.10.0:GHC.Types.Char]"
-      , "[[ghc-prim-0.10.0:GHC.Types.Char]]"
-      , "base-4.18.0.0:GHC.Real.Ratio ghc-bignum-1.3:GHC.Num.Integer.Integer"
-      , "scientific-0.3.7.0:Data.Scientific.Scientific"
-      , "bytestring-0.11.4.0:Data.ByteString.Builder.Internal.Builder"
-      , "[base-4.18.0.0:GHC.Word.Word8]"
-      , "[ghc-prim-0.10.0:GHC.Types.Bool]"
-      , "base-4.18.0.0:GHC.Maybe.Maybe ghc-prim-0.10.0:GHC.Types.Bool"
-      ]
-  , FunGraph.buildConfig_excludeTypesUnqualified = Set.fromList
-      [ "String"
-      , "Char"
-      , "FastString"
-      , "ShortByteString"
-      , "Bool"
-      , "Bool#"
-      , "Int"
-      , "Int32"
-      , "Int64"
-      , "Word"
-      , "Word8"
-      , "Word16"
-      , "Word32"
-      , "Word64"
-      , "Float"
-      , "Double"
-      , "Integer"
-      , "ByteString"
-      , "Text"
-      , "ByteArray#"
-      ]
-  , FunGraph.buildConfig_excludeModulePatterns = Set.fromList
-      [ "Foreign.C.Types" -- TODO: verify that this works (it seems to not work)
-      , "Codec.CBOR"
-      ]
-  }
+import qualified Types.Doodle
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import Data.Foldable (forM_)
+import qualified System.Console.ANSI as ANSI
+import qualified System.Environment as Args
+import qualified FunGraph.Build
 
 main :: IO ()
 main = do
-  [fileName, timoutMillisStr] <- Env.getArgs
-  timoutMillis <- maybe (fail $ "Invalid timeout: " <> show timoutMillisStr) pure (readMaybe timoutMillisStr)
-  FunGraph.withGraphFromFile (FunGraph.defaultBuildConfig <> buildConfig) fileName $ \graph -> do
-    vertices <- ST.stToIO $ DG.vertexLabels graph
-    forM_ vertices $ \src ->
-      forM_ vertices $ \dst -> do
-        res <- query timoutMillis graph src dst
-        case res of
-          [] -> pure ()
-          (fns,_):_ -> when (length fns > 9) $ do
-            putStrLn . ((show (length fns) <> " ") <>) $
-              let fns' = map NE.head fns
-              in mconcat
-                [ color ANSI.Green $
-                    T.unpack $
-                      FunGraph.renderFullyQualifiedType src <> " -> " <> FunGraph.renderFullyQualifiedType dst
-                , " : "
-                , color ANSI.Red $ renderTypeSig fns'
-                , " : "
-                , color ANSI.Blue $ FunGraph.renderComposedFunctionsStr fns'
-                ]
+  [fileName] <- Args.getArgs
+  graphData <- either fail pure =<< FunGraph.Build.fileReadDeclarationMap fileName
+  tmpPrintExtendedFunctions graphData
+
+tmpPrintExtendedFunctions
+  :: [Json.DeclarationMapJson T.Text]
+  -> IO ()
+tmpPrintExtendedFunctions declarationMapJsonLst =
+  traceIt
+    (Types.Doodle.extendFrom monoFuns polyFuns)
   where
-    maxCount = 1
+    extract
+      :: Json.DeclarationMapJson T.Text
+      -> [(T.Text, Types.Doodle.SomeFunction)] -- (fully qualified name, type). e.g. ("Data.List.head", "[a] -> a")
+    extract =
+        concatMap
+          (\(moduleName, nameToTypeMap) ->
+            let assocs = Map.assocs nameToTypeMap
+                funMetaData (funName, funType) = T.pack $ unlines
+                  [ ("\t" <>) $ color ANSI.Red $ T.unpack $ moduleName <> "." <> funName
+                  , "\t\t" <> " :: " <> color ANSI.Blue (T.unpack $ Types.Doodle.renderSomeFunctionType funType)
+                  ]
+            in map (\(funName, funType) -> (funMetaData (funName, funType), funType)) assocs
+          )
+      . Map.assocs
+      . Json.moduleDeclarations_map
+      . Json.declarationMapJson_moduleDeclarations
 
-    renderTypeSig = T.unpack . FunGraph.Util.showTypeSig . FunGraph.Util.typedFunctionsPathTypes
+    someFunctions = concatMap extract declarationMapJsonLst
 
-    query timoutMillis graph src dst = fmap (fromMaybe []) $
-      System.Timeout.timeout (timoutMillis * 1000) $
-        ST.stToIO $
-          fmap (either (error . show) id) $
-            FunGraph.runGraphAction graph $ FunGraph.queryTreeGA maxCount (src, dst)
+    monoFuns :: [(T.Text, Types.FunctionType (FgType (Types.FgTyCon T.Text)))]
+    monoFuns =
+      Data.Maybe.mapMaybe (traverse Types.Doodle.someFunctionMonomorphic) someFunctions
+
+    polyFuns :: [(T.Text, Types.Doodle.FunctionTypeForall T.Text T.Text)]
+    polyFuns =
+      Data.Maybe.mapMaybe (traverse Types.Doodle.someFunctionPolymorphic) someFunctions
+
+    traceIt
+      :: [((T.Text, T.Text), Either String (Types.FunctionType (FgType (Types.FgTyCon T.Text))))]
+      -> IO ()
+    traceIt ftl = do
+      putStrLn ""
+      forM_ ftl $ \blah -> putStrLn $ mkTraceString blah
+
+    mkTraceString ((monoName, polyName), eTypedFunction) =
+      let resultType =
+            either
+              ("ERROR: " <>)
+              (\ft -> ":: " <> color ANSI.Blue (T.unpack (renderFunctionType ft)))
+              eTypedFunction
+      in
+      unlines
+        [ "Extending from"
+        , T.unpack monoName
+        , "\t" <> "with"
+        , T.unpack polyName
+        , "\t" <> "resulting in"
+        , "\t\t" <> resultType
+        , ""
+        ]
 
     color :: ANSI.Color -> String -> String
     color color' str = concat
-        [ ANSI.setSGRCode [ANSI.SetColor ANSI.Foreground ANSI.Dull color']
-        , str
-        , ANSI.setSGRCode [ANSI.Reset]
+      [ ANSI.setSGRCode [ANSI.SetColor ANSI.Foreground ANSI.Dull color']
+      , str
+      , ANSI.setSGRCode [ANSI.Reset]
+      ]
+
+    renderFunctionType
+      :: Types.FunctionType (FgType (Types.FgTyCon T.Text))
+      -> T.Text
+    renderFunctionType ft =
+      T.unwords
+        [ Types.renderFgTypeFgTyConQualifiedNoPackage $ Types.functionType_arg ft
+        , "->"
+        , Types.renderFgTypeFgTyConQualifiedNoPackage $ Types.functionType_ret ft
         ]
