f213303a52f4ee407215dc65aeba6b3fef5fd931
Rune K. Svendsen <runesvend@gmail.com>
2025-04-19 12:24:11 +0200

    Remove unused `traceFun` from Server.Config

--- src/server/Server/Config.hs
+++ src/server/Server/Config.hs
@@ -14,8 +14,6 @@ where
 import qualified Server.Pages.Search
 import Lucid (Html)
 import qualified FunGraph.Build
-import qualified GHC.IO.Unsafe
-import qualified System.Console.ANSI as ANSI
 
 data ServerConfig = ServerConfig
   { serverConfigPort :: Int
@@ -35,17 +33,3 @@ mkDefaultServerConfig graphDataFilename = ServerConfig
   , serverConfigSearchConfig = Server.Pages.Search.defaultSearchConfig
   , serverConfigBuildConfig = FunGraph.Build.defaultBuildConfig
   }
-
-traceFun
-  :: Applicative f
-  => String
-  -> f ()
-traceFun str =
-  let color color' = concat
-        [ ANSI.setSGRCode [ANSI.SetColor ANSI.Background ANSI.Dull color']
-        , str
-        , ANSI.setSGRCode [ANSI.Reset]
-        ]
-  in pure $
-    GHC.IO.Unsafe.unsafePerformIO $ do
-      putStrLn $ color ANSI.Red
